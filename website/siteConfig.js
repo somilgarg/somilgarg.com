a8efeec657098bb885fc11d65c5f44fdade45116
somilgarg181 <somilgarg181@gmail.com>
2019-10-08 16:45:35 +0530

    changes

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,10 +21,10 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'Somil Garg', // Title for your website.
+  tagline: 'Software Engineer',
   url: 'https://somilgarg.com', // Your website URL
-  baseUrl: '/somilgarg.com/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
